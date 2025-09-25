850a16dbcccfbd86e1c7f31f0c88a1157a3ffbbb
nimurr <nimurnerob404@gmail.com>
2025-09-25 12:33:15 +0600

    "Removed unused variable and added useRouter hook"

--- src/Dashboard/sidebarMenu/specialist/TrainingPrograms.jsx
+++ src/Dashboard/sidebarMenu/specialist/TrainingPrograms.jsx
@@ -14,7 +14,6 @@ export default function TrainingPrograms() {
   const { data } = useGetAllTrainingProgramQuery(pageNumber);
   const programs = data?.data?.attributes?.results || [];
 
-
   const router = useRouter()
 
   return (
