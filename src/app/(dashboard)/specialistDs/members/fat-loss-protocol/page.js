0928097ff639fb23b3101db1d65287a6a195ea7e
nimurr <nimurnerob404@gmail.com>
2025-10-30 13:28:55 +0600

    `Added isLoading state to useGetMealPlanByProtocolIdAndPatientIdQuery hook and displayed a loading message when it's true.`

--- src/app/(dashboard)/specialistDs/members/fat-loss-protocol/page.js
+++ src/app/(dashboard)/specialistDs/members/fat-loss-protocol/page.js
@@ -17,7 +17,7 @@ const Page = () => {
         }
     }, []); // This runs only once, after the component mounts on the client side
 
-    const { data } = useGetMealPlanByProtocolIdAndPatientIdQuery({ protocolId, patientId });
+    const { data, isLoading } = useGetMealPlanByProtocolIdAndPatientIdQuery({ protocolId, patientId });
     const mealPlanData = data?.data?.attributes?.results || [];
     console.log("Meal Plan Data:", mealPlanData);
 
@@ -27,7 +27,7 @@ const Page = () => {
         <div>
             <h2 className='text-2xl font-semibold my-5'>All Plans</h2>
             <p>Click a Plan to give a suggestion </p>
-            <div className=' bg-white rounded-lg shadow p-8'>
+            <div className=' bg-white rounded-lg border border-gray-200 mt-2 p-5'>
                 {
                     mealPlanData?.map((plan) => (
                         <Link href={`/specialistDs/members/mealplan?planByDoctorId=${plan._planByDoctorId}`} key={plan._id} className='flex justify-between rounded hover:bg-blue-50 gap-20 border w-full p-2 mb-3'>
@@ -37,7 +37,7 @@ const Page = () => {
                     ))
                 }
                 {
-
+                    isLoading && <p className='text-center font-semibold text-blue-600'>Loading...</p>
                 }
             </div>
         </div>
